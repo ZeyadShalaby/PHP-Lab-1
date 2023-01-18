a8c5f9962fc8d6f77d2f8df20ec1b766f4c84370
ZeyadShalaby <104474463+ZeyadShalaby@users.noreply.github.com>
2023-01-18 23:49:14 +0200

    Lab 1

---
+++ task1.php
@@ -0,0 +1,34 @@
+<?php
+
+#echo phpinfo();
+
+define("WEBSITE_NAME", "Website Title");
+
+echo WEBSITE_NAME."<br>";
+
+
+echo __FILE__."<br>";
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+
+$age = 10;
+
+if ($age < 5){
+    echo "stay at home";
+}else if ($age = 5){
+    echo "go to kindergarten";
+}elseif ($age >5){
+    echo "go to grade".$age-6;
+}
+
+?>

e8f77a04a60678b6b66b3c9ed503e652fbd27a5c
ZeyadShalaby <104474463+ZeyadShalaby@users.noreply.github.com>
2023-01-20 14:07:01 +0200

    Update task1.php

--- task1.php
+++ task1.php
@@ -21,13 +21,13 @@ echo "<br>";
 echo $_SERVER['SCRIPT_NAME'];
 echo "<br>";
 
-$age = 10;
+$age = 4;
 
 if ($age < 5){
     echo "stay at home";
 }else if ($age = 5){
     echo "go to kindergarten";
-}elseif ($age >5){
+}elseif ($age >5 && $age<12){
     echo "go to grade".$age-6;
 }
 
